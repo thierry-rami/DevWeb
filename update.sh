ea6e9345ceb3e03437499541e9911261771f166f
thierry-rami <thierry.rami@laplateforme.io>
2023-08-06 22:19:24 +0200

    script update config

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir /srv/opt
+sudo echo "/opt /srv/opt none rw,bind 0 0" > /etc/fstab
+sudo systemctl daemon-reload
+sudo mount -a
+
+sudo  rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+pip3 list -o | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip3 install -U
+
+
